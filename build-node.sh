219ce7b9455092e1477c0aea067d5935a9120b4d
Tim Rodger <tim.rodger@gmail.com>
2015-05-15 00:22:02 +0100

    Make shallow git clones

--- build-node.sh
+++ build-node.sh
@@ -7,15 +7,17 @@
 NODE_TMPL=git@github.com:ArtCoeur/node-template.git
 APP_REPO=$1
 
-git clone $NODE_TMPL
+git clone --depth 1 $NODE_TMPL
+
+rm -rf node-template/.git
 
 cd node-template/src
 
-git clone $APP_REPO
+git clone --depth 1 $APP_REPO
 
 # expect a lib directory - test this here?
 
 cd ../
 
-docker build --tag=$2 .
+docker build -t $2 .
 
