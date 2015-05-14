120d2a8942b5217aa05d9987139f63e04e8fd52d
Tim Rodger <tim.rodger@gmail.com>
2015-05-14 23:59:56 +0100

    Update build-node.sh

--- build-node.sh
+++ build-node.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 # Build a node container using the node template
+# Application repo is arg 1
+# Container name is arg 2
+
+NODE_TMPL=git@github.com:ArtCoeur/node-template.git
+APP_REPO=$1
+
+git clone $NODE_TMPL
+
+cd node-template/src
+
+git clone $APP_REPO
+
+# expect a lib directory - test this here?
+
+cd ../
+
+docker build --tag=$2 .
+
